0fddda2186916f37b900e68db49ab692c2083171
Matthieu Tricoire <matthieutricoire@Matthieus-MacBook-Pro.local>
2024-04-18 15:39:28 +0100

    add functions ton fzf

--- zsh/.zsh.d/functions.zsh
+++ zsh/.zsh.d/functions.zsh
@@ -15,3 +15,31 @@ function nvims() {
   NVIM_APPNAME=$config nvim $@
 }
 
+# Use fd (https://github.com/sharkdp/fd) for listing path candidates.
+# - The first argument to the function ($1) is the base path to start traversal
+# - See the source code (completion.{bash,zsh}) for the details.
+function _fzf_compgen_path() {
+  echo "test"
+  fd --hidden --exclude .git . "$1"
+}
+
+# Use fd to generate the list for directory completion
+function _fzf_compgen_dir() {
+  fd --type=d --hidden --exclude .git . "$1"
+}
+
+# Advanced customization of fzf options via _fzf_comprun function
+# - The first argument to the function is the name of the command.
+# - You should make sure to pass the rest of the arguments to fzf.
+function _fzf_comprun() {
+  local command=$1
+  shift
+
+  case "$command" in
+    cd)           fzf --preview 'eza --tree --color=always {} | head -200' "$@" ;;
+    export|unset) fzf --preview "eval 'echo $'{}"         "$@" ;;
+    ssh)          fzf --preview 'dig {}'                   "$@" ;;
+    *)            fzf --preview "bat -n --color=always --line-range :500 {}" "$@" ;;
+  esac
+}
+
