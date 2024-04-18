cda98054ec42e7427cb2b3b9ddd715bcf123b464
Matthieu Tricoire <matthieutricoire@Matthieus-MacBook-Pro.local>
2024-04-18 15:38:32 +0100

    add keymap to jump lines 5 by 5

--- nvim/.config/lazyvim/lua/config/keymaps.lua
+++ nvim/.config/lazyvim/lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<Up>", "5k", { desc = "Up to 5 lines" })
+vim.keymap.set("n", "<Down>", "5j", { desc = "Down to 5 lines" })
